748220ae56e09669f219142bfb6a1cfe6e301037
kbdunn07 <157818211+kbdunn07@users.noreply.github.com>
2026-02-25 09:46:41 -0600

    Update Transactions.java

--- Transactions.java
+++ Transactions.java
@@ -1,5 +1,40 @@
 package javaPackage;
 
 public class Transactions {
-
+	double amount;
+	String type; // Purchase, Deposit, Withdrawal, Transfer
+	Accounts account;
+	Cards card;
+	
+	public Transactions(double amount, Accounts account, String type, Cards card) {
+		this.amount = amount;
+		this.type = type;
+		this.account = account;
+		this.card = card;
+		
+		if (type.equalsIgnoreCase("Deposit")) {
+			account.setBalance(amount + account.balance);
+		}
+		
+		else if (type.equalsIgnoreCase("Withdrawal")) {
+			account.setBalance(account.balance - amount);
+		}
+		
+		else if (type.equalsIgnoreCase("Purchase")) {
+			account.setBalance(account.balance - amount);
+		}
+		
+		account.transactions.add(this);
+		card.transactions.add(this);
+		
+	}
+	
+	public Transactions(double amount, Accounts account, String type) {
+		this.amount = amount;
+		this.type = type;
+		this.account = account;
+	}
+	
+	
+	
 }
