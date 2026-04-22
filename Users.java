83016641c6aa00d4488f5fdc1bcaa89307ff24fe
kbdunn07 <157818211+kbdunn07@users.noreply.github.com>
2026-04-21 19:45:24 -0500

    Update Users.java

--- Users.java
+++ Users.java
@@ -1,30 +1,42 @@
-package javaPackage;
-import javaPackage.Accounts;
-import javaPackage.Cards;
+package BankingPackage;
 import java.util.ArrayList;
 
 
 public class Users {
-	String name;
-	String password;
-	ArrayList<Accounts> accounts = new ArrayList<>();
-	ArrayList<Cards> cards = new ArrayList<>();
-	
-	public Users(String name, String password) {
-		this.name = name;
-		this.password = password;
-	}
-	
-	public String getName() {
-		return name;
-	}
-	
-	public String display() {
-		String result = name + " | " + password + "\n";
-		for (Accounts a : accounts) {
-			result += "\n" + a.display();
-		}
-		return result;
-	}
-	
+    String username;
+    String password;
+
+    ArrayList<Accounts> accounts = new ArrayList<>();
+    ArrayList<Cards> cards = new ArrayList<>();
+
+    public Users(String username, String password) {
+
+        this.username = username;
+        this.password = password;
+    }
+
+    public ArrayList<Accounts> getAccounts() {
+        return accounts;
+    }
+
+    public void addAccount(Accounts account) {
+        accounts.add(account);
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String display() {
+        String result = username + " | " + password + "\n";
+        for (Accounts a : accounts) {
+            result += "\n" + a.display();
+        }
+        return result;
+    }
+
 }
