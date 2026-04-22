f8cfa264526799b59f66ccd1b90548928c38f669
kbdunn07 <157818211+kbdunn07@users.noreply.github.com>
2026-04-21 19:44:51 -0500

    Update Accounts.java

--- Accounts.java
+++ Accounts.java
@@ -1,49 +1,46 @@
-package javaPackage;
-import javaPackage.Cards;
-import javaPackage.Users;
-import javaPackage.Transactions;
+package BankingPackage;
 import java.util.ArrayList;
 
 
 public class Accounts {
-	Users user;
-	String accName;
-	int accNum;
-	int routeNum;
-	double balance;
-	ArrayList<Cards> cards = new ArrayList<>();
-	ArrayList<Transactions> transactions = new ArrayList<>();
-	
-	public Accounts(Users user, String accName, double balance) {
-		this.user = user;
-		this.accName = accName;
-		this.balance = balance;
-		this.accNum = (int)(Math.random() * (999999999 + 1));
-		this.routeNum = (int)(Math.random() * (999999999 + 1));
-		user.accounts.add(this);
-	}
-	
-	public void setBalance(double balance) {
-		this.balance = balance;
-	}
-	
-	public double getBalance() {
-		return this.balance;
-	}
-	
-	public String getAccName() {
-		return this.accName;
-	}
-	
-	
-	public String display() {
-		String result = "";
-		result += accName + " | " + user.getName() + "\nAccount Number: " + accNum + "\nRouting Number: " + routeNum + "\n$" + balance + "\n";
-		result += "Cards on Account:\n";
-		for (Cards c : cards) {
-			result += c.display();
-		}
-		return result;
-	}
-	
+    Users user;
+    String accName;
+    int accNum;
+    int routeNum;
+    double balance;
+    ArrayList<Cards> cards = new ArrayList<>();
+    ArrayList<Transactions> transactions = new ArrayList<>();
+
+    public Accounts(Users user, String accName, double balance) {
+        this.user = user;
+        this.accName = accName;
+        this.balance = balance;
+        this.accNum = (int)(Math.random() * (999999999 + 1));
+        this.routeNum = (int)(Math.random() * (999999999 + 1));
+        //user.addAccount(this);
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getBalance() {
+        return this.balance;
+    }
+
+    public String getAccName() {
+        return this.accName;
+    }
+
+
+    public String display() {
+        String result = "";
+        result += accName + " | " + user.getUsername() + "\nAccount Number: " + accNum + "\nRouting Number: " + routeNum + "\n$" + balance + "\n";
+        result += "Cards on Account:\n";
+        for (Cards c : cards) {
+            result += c.display();
+        }
+        return result;
+    }
+
 }
