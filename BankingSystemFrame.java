9999e5898dc19ae81111a62ce8f826ac6901245e
kbdunn07 <157818211+kbdunn07@users.noreply.github.com>
2026-04-21 19:44:29 -0500

    Update BankingSystemFrame.java

--- BankingSystemFrame.java
+++ BankingSystemFrame.java
@@ -1,75 +1,489 @@
-package javaPackage;
+package BankingPackage;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
-public class BankingSystemFrame extends JFrame implements ActionListener {
-	
-	CardLayout mainCard;
-	GridBagConstraints layout = null;
-	JLabel loginLabel;
-	JLabel usernameLabel;
-	JLabel passwordLabel;
-	JTextField usernameField;
-	JTextField passwordField;
-	JButton loginButton;
-	JButton createAccountButton;
-	JPanel mainPanel;
-	
-	BankingSystemFrame() {
-		setTitle("Banking System");
-		mainCard = new CardLayout();
-		mainPanel = new JPanel(mainCard);
-		
-		JPanel loginPage = createLoginPage();
-		JPanel createAccountPage = createAccountPage();
-		
-		mainPanel.add(loginPage, "login");
-		mainPanel.add(createAccountPage, "create account");
-		add(mainPanel);
-		
-	}
-	
-	private JPanel createAccountPage() {
-		JPanel panel = new JPanel();
+//real coders don't use comments so they have no idea what they are coding when they come back
 
-        JButton backButton = new JButton("Back");
-        backButton.addActionListener(e -> 
-        {
-        	mainCard.show(mainPanel, "login");
-        });
+public class BankingSystemFrame extends JFrame {
 
-        panel.add(new JLabel("Create Account Page"));
-        panel.add(backButton);
+    private CardLayout mainCard;
+    private JPanel mainPanel;
+    private ArrayList<Users> users = new ArrayList<>();
+    private Users currentUser;
 
-        return panel;
-	}
-	
-	private JPanel createLoginPage() {
-		JPanel panel = new JPanel();
-		createAccountButton = new JButton("Create New Account");
-		createAccountButton.addActionListener(e -> 
-		{
-			mainCard.show(mainPanel, "create account");
-		});
-		panel.add(createAccountButton);
-		
-		return panel;
-		
-	}
-	
-	@Override
-	public void actionPerformed(ActionEvent event) {
-		
-		
-	}
-	
-	public static void main(String[] args) {
-        BankingSystemFrame frame = new BankingSystemFrame();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(1920, 1080);
-        frame.setVisible(true);
+    public BankingSystemFrame() {
+        setTitle("Banking System");
+
+        mainCard = new CardLayout();
+        mainPanel = new JPanel(mainCard);
+
+        mainPanel.add(createLoginPage(), "login");
+        mainPanel.add(createAccountPage(), "create");
+        //mainPanel.add(createUserHomePage(), "user home");
+        mainPanel.add(createLoggedInAccountPage(), "create account from home");
+
+        add(mainPanel);
     }
 
+    private void openAccountPage(Accounts acc) {
+
+        JPanel panel = new JPanel(new BorderLayout());
+
+        JLabel title = new JLabel(acc.getAccName(), SwingConstants.CENTER);
+        title.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 24));
+
+        JLabel info = new JLabel(
+                "Account #: " + acc.accNum +
+                        " | Balance: $" + acc.getBalance(),
+                SwingConstants.CENTER
+        );
+
+        JPanel top = new JPanel(new GridLayout(2, 1));
+        top.add(title);
+        top.add(info);
+
+        JPanel transactionsPanel = new JPanel();
+        transactionsPanel.setLayout(new BoxLayout(transactionsPanel, BoxLayout.Y_AXIS));
+
+        JScrollPane scroll = new JScrollPane(transactionsPanel);
+
+        JButton addTransaction = new JButton("Add Transaction");
+
+        addTransaction.addActionListener(e -> {
+            JPanel txPage = createAddTransactionPage(acc);
+            mainPanel.add(txPage, "add transaction");
+            mainCard.show(mainPanel, "add transaction");
+        });
+
+        for (Transactions t : acc.transactions) {
+            JLabel label;
+            if (t.type.equalsIgnoreCase("Deposit")) {
+                label = new JLabel("Deposit: +$" + t.amount);
+            } else {
+                label = new JLabel(t.type + ": -$" + t.amount);
+            }
+
+            transactionsPanel.add(label);
+        }
+
+        panel.add(top, BorderLayout.NORTH);
+        panel.add(scroll, BorderLayout.CENTER);
+        panel.add(addTransaction, BorderLayout.SOUTH);
+
+        mainPanel.add(panel, "account page");
+        mainCard.show(mainPanel, "account page");
+    }
+
+    private Users authenticate(String username, String password) {
+        for (Users user : users) {
+            if (user.getUsername().equals(username) &&
+                    user.getPassword().equals(password)) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    private JPanel createLoginPage() {
+        JPanel panel = new JPanel(new GridBagLayout());
+        GridBagConstraints layout = new GridBagConstraints();
+
+        JTextField usernameField = new JTextField(15);
+        JTextField passwordField = new JTextField(15);
+        JLabel usernameLabel = new JLabel("Username:");
+        JLabel passwordLabel = new JLabel("Password:");
+        JLabel header = new JLabel("Account Login");
+
+        JButton loginButton = new JButton("Login");
+        JButton createAccountButton = new JButton("Create New Account");
+
+        loginButton.addActionListener(e -> {
+            Users user = authenticate(usernameField.getText(), passwordField.getText());
+
+            if (user != null) {
+                JOptionPane.showMessageDialog(this, "Welcome, " + user.getUsername() + "!");
+                currentUser = user;
+                mainPanel.add(createUserHomePage(), "user home");
+                mainCard.show(mainPanel, "user home");
+            } else {
+                JOptionPane.showMessageDialog(this, "Username or Password is incorrect.");
+                usernameField.setText("");
+                passwordField.setText("");
+            }
+        });
+
+        createAccountButton.addActionListener(e -> {
+            mainCard.show(mainPanel, "create");
+        });
+
+        layout.insets = new Insets(10, 10, 10, 10);
+        layout.fill = GridBagConstraints.HORIZONTAL;
+
+        header.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 28));
+        header.setForeground(new Color(0, 0, 140));
+        header.setHorizontalAlignment(SwingConstants.CENTER);
+
+        layout.gridx = 0;
+        layout.gridy = 0;
+        layout.gridwidth = 2;
+        panel.add(header, layout);
+
+        layout.gridwidth = 1;
+
+        layout.gridx = 0;
+        layout.gridy = 1;
+        layout.anchor = GridBagConstraints.EAST;
+        panel.add(usernameLabel, layout);
+
+        layout.gridx = 1;
+        layout.anchor = GridBagConstraints.WEST;
+        panel.add(usernameField, layout);
+
+        layout.gridx = 0;
+        layout.gridy = 2;
+        layout.anchor = GridBagConstraints.EAST;
+        panel.add(passwordLabel, layout);
+
+        layout.gridx = 1;
+        layout.anchor = GridBagConstraints.WEST;
+        panel.add(passwordField, layout);
+
+        layout.gridx = 0;
+        layout.gridy = 3;
+        layout.gridwidth = 2;
+        layout.anchor = GridBagConstraints.CENTER;
+        panel.add(loginButton, layout);
+
+        layout.gridy = 4;
+        panel.add(createAccountButton, layout);
+
+        return panel;
+    }
+
+    private JPanel createAccountPage() {
+        JPanel panel = new JPanel(new GridBagLayout());
+        GridBagConstraints layout = new GridBagConstraints();
+
+        JTextField newUsername = new JTextField(15);
+        JTextField newPassword = new JTextField(15);
+
+        JButton createButton = new JButton("Create Account");
+        JButton backButton = new JButton("Back");
+
+        createButton.addActionListener(e -> {
+            boolean created = false;
+            String username = newUsername.getText();
+            String password = newPassword.getText();
+
+            if (username.isEmpty() || password.isEmpty()) {
+                JOptionPane.showMessageDialog(this, "Please enter all fields!");
+                return;
+            }
+
+            for (Users user : users) {
+                if (user.getUsername().equals(username)) {
+                    JOptionPane.showMessageDialog(this, "Username already exists.");
+                    return;
+                }
+            }
+
+            users.add(new Users(username, password));
+            JOptionPane.showMessageDialog(this, "Account has been created.");
+            created = true;
+
+            if (created) {
+                mainCard.show(mainPanel, "login");
+            }
+        });
+
+        backButton.addActionListener(e -> {
+            mainCard.show(mainPanel, "login");
+        });
+
+        layout.insets = new Insets(10, 10, 10, 10);
+        layout.fill = GridBagConstraints.HORIZONTAL;
+
+        JLabel header = new JLabel("Create Account");
+
+        header.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 28));
+        header.setForeground(new Color(0, 0, 140));
+        header.setHorizontalAlignment(SwingConstants.CENTER);
+
+        layout.gridx = 0;
+        layout.gridy = 0;
+        layout.gridwidth = 2;
+        panel.add(header, layout);
+
+        layout.gridwidth = 1;
+
+        JLabel usernameLabel = new JLabel("Username:");
+        JLabel passwordLabel = new JLabel("Password:");
+
+        layout.gridx = 0;
+        layout.gridy = 1;
+        layout.anchor = GridBagConstraints.EAST;
+        panel.add(usernameLabel, layout);
+
+        layout.gridx = 1;
+        layout.anchor = GridBagConstraints.WEST;
+        panel.add(newUsername, layout);
+
+        layout.gridx = 0;
+        layout.gridy = 2;
+        layout.anchor = GridBagConstraints.EAST;
+        panel.add(passwordLabel, layout);
+
+        layout.gridx = 1;
+        layout.anchor = GridBagConstraints.WEST;
+        panel.add(newPassword, layout);
+
+        layout.gridx = 0;
+        layout.gridy = 3;
+        layout.gridwidth = 2;
+        layout.anchor = GridBagConstraints.CENTER;
+        panel.add(createButton, layout);
+
+        layout.gridy = 4;
+        panel.add(backButton, layout);
+
+        return panel;
+    }
+
+    private JPanel createUserHomePage() {
+        JPanel panel = new JPanel(new BorderLayout());
+
+        JLabel header = new JLabel();
+        header.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 24));
+        header.setForeground(new Color(0, 0, 140));
+        header.setHorizontalAlignment(SwingConstants.CENTER);
+
+        JPanel accountsPanel = new JPanel();
+        accountsPanel.setLayout(new GridLayout(0,3,5,5));
+
+        JScrollPane scrollPane = new JScrollPane(accountsPanel);
+
+        JButton createAccountButton = new JButton("Create New Account");
+
+        createAccountButton.addActionListener(e -> {
+            mainCard.show(mainPanel, "create account from home");
+        });
+
+        if (currentUser != null) {
+            header.setText("Welcome, " + currentUser.getUsername());
+
+            if (currentUser.getAccounts().isEmpty()) {
+                JLabel empty = new JLabel("No accounts found.");
+                empty.setAlignmentX(Component.CENTER_ALIGNMENT);
+                accountsPanel.add(empty);
+            } else {
+                for (Accounts acc : currentUser.getAccounts()) {
+
+                    JPanel card = new JPanel();
+                    card.setCursor(new Cursor(Cursor.HAND_CURSOR));
+                    card.setLayout(new BoxLayout(card, BoxLayout.Y_AXIS));
+                    card.setBorder(BorderFactory.createCompoundBorder(
+                            BorderFactory.createTitledBorder(acc.getAccName()),
+                            BorderFactory.createEmptyBorder(5,10,5,10)
+                    ));
+                    card.setPreferredSize(new Dimension(120, 100));
+
+                    card.addMouseListener(new java.awt.event.MouseAdapter() {
+                        public void mouseClicked(java.awt.event.MouseEvent e) {
+                            openAccountPage(acc);
+                        }
+                    });
+
+                    card.add(new JLabel("Account #: " + acc.accNum));
+                    card.add(new JLabel("Routing #: " + acc.routeNum));
+                    card.add(new JLabel("Name: " + acc.getAccName()));
+                    card.add(new JLabel("Balance: $" + acc.getBalance()));
+
+                    accountsPanel.add(card);
+                }
+                accountsPanel.revalidate();
+                accountsPanel.repaint();
+            }
+        } else {
+            header.setText("No user logged in");
+        }
+
+        panel.add(header, BorderLayout.NORTH);
+        panel.add(scrollPane, BorderLayout.CENTER);
+        JPanel buttonPanel = new JPanel();
+
+        if (currentUser != null) {
+            buttonPanel.add(createAccountButton);
+        }
+
+        panel.add(buttonPanel, BorderLayout.SOUTH);
+        return panel;
+    }
+
+    private JPanel createAddTransactionPage(Accounts acc) {
+
+        JPanel panel = new JPanel(new GridBagLayout());
+        GridBagConstraints layout = new GridBagConstraints();
+
+        JTextField amountField = new JTextField(15);
+
+        String[] types = {"Deposit", "Withdrawal", "Purchase"};
+        JComboBox<String> typeBox = new JComboBox<>(types);
+
+        JButton submitButton = new JButton("Submit");
+        JButton backButton = new JButton("Back");
+
+        layout.insets = new Insets(10, 10, 10, 10);
+        layout.fill = GridBagConstraints.HORIZONTAL;
+
+        JLabel header = new JLabel("Add Transaction");
+        header.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 24));
+        header.setHorizontalAlignment(SwingConstants.CENTER);
+
+        layout.gridx = 0;
+        layout.gridy = 0;
+        layout.gridwidth = 2;
+        panel.add(header, layout);
+
+        layout.gridwidth = 1;
+
+        layout.gridx = 0;
+        layout.gridy = 1;
+        panel.add(new JLabel("Amount:"), layout);
+
+        layout.gridx = 1;
+        panel.add(amountField, layout);
+
+        layout.gridx = 0;
+        layout.gridy = 2;
+        panel.add(new JLabel("Type:"), layout);
+
+        layout.gridx = 1;
+        panel.add(typeBox, layout);
+
+        submitButton.addActionListener(e -> {
+            try {
+                double amount = Double.parseDouble(amountField.getText());
+                String type = (String) typeBox.getSelectedItem();
+
+                if ((type.equalsIgnoreCase("Withdrawal") || type.equalsIgnoreCase("Purchase"))
+                        && amount > acc.getBalance()) {
+
+                    JOptionPane.showMessageDialog(this, "Insufficient funds.");
+                    return;
+                }
+                new Transactions(amount, acc, type, null);
+
+                JOptionPane.showMessageDialog(this, "Transaction added!");
+
+                openAccountPage(acc);
+
+            } catch (IllegalArgumentException ex) {
+                JOptionPane.showMessageDialog(this, ex.getMessage());
+            }
+            catch (Exception ex) {
+                JOptionPane.showMessageDialog(this, "Invalid input.");
+            }
+        });
+
+        backButton.addActionListener(e -> {
+            openAccountPage(acc);
+        });
+
+        layout.gridx = 0;
+        layout.gridy = 3;
+        layout.gridwidth = 2;
+        panel.add(submitButton, layout);
+
+        layout.gridy = 4;
+        panel.add(backButton, layout);
+
+        return panel;
+    }
+
+    private JPanel createLoggedInAccountPage() {
+        JPanel panel = new JPanel(new GridBagLayout());
+        GridBagConstraints layout = new GridBagConstraints();
+
+        JTextField accountNameField = new JTextField(15);
+        JTextField balanceField = new JTextField(15);
+
+        JButton createButton = new JButton("Create Account");
+        JButton backButton = new JButton("Back");
+
+        layout.insets = new Insets(10, 10, 10, 10);
+        layout.fill = GridBagConstraints.HORIZONTAL;
+
+        JLabel header = new JLabel("Create Bank Account");
+        header.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 28));
+        header.setForeground(new Color(0, 0, 140));
+        header.setHorizontalAlignment(SwingConstants.CENTER);
+
+        layout.gridx = 0;
+        layout.gridy = 0;
+        layout.gridwidth = 2;
+        panel.add(header, layout);
+
+        layout.gridwidth = 1;
+
+        JLabel nameLabel = new JLabel("Account Name:");
+        JLabel balanceLabel = new JLabel("Starting Balance:");
+
+        layout.gridx = 0;
+        layout.gridy = 1;
+        panel.add(nameLabel, layout);
+
+        layout.gridx = 1;
+        panel.add(accountNameField, layout);
+
+        layout.gridx = 0;
+        layout.gridy = 2;
+        panel.add(balanceLabel, layout);
+
+        layout.gridx = 1;
+        panel.add(balanceField, layout);
+
+        createButton.addActionListener(e -> {
+            try {
+                String name = accountNameField.getText();
+                double balance = Double.parseDouble(balanceField.getText());
+
+                if (currentUser != null) {
+                    Accounts acc = new Accounts(currentUser, name, balance);
+                    currentUser.addAccount(acc);
+                    accountNameField.setText("");
+                    balanceField.setText("");
+                }
+                mainPanel.add(createUserHomePage(), "user home");
+                mainCard.show(mainPanel, "user home");
+
+            } catch (Exception ex) {
+                JOptionPane.showMessageDialog(this, "Invalid input.");
+            }
+        });
+
+        backButton.addActionListener(e -> {
+            mainCard.show(mainPanel, "user home");
+        });
+
+        layout.gridx = 0;
+        layout.gridy = 3;
+        layout.gridwidth = 2;
+        panel.add(createButton, layout);
+
+        layout.gridy = 4;
+        panel.add(backButton, layout);
+
+        return panel;
+
+    }
+
+    public static void main(String[] args) {
+
+        BankingSystemFrame mainFrame = new BankingSystemFrame();
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainFrame.setSize(600, 600);
+        mainFrame.setVisible(true);
+
+    }
 }
