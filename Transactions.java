7d7d8f59143d820a8936a8b962a1aaf3b4985d02
kbdunn07 <157818211+kbdunn07@users.noreply.github.com>
2026-04-21 19:45:11 -0500

    Update Transactions.java

--- Transactions.java
+++ Transactions.java
@@ -1,40 +1,32 @@
-package javaPackage;
+package BankingPackage;
 
 public class Transactions {
-	double amount;
-	String type; // Purchase, Deposit, Withdrawal, Transfer
-	Accounts account;
-	Cards card;
-	
-	public Transactions(double amount, Accounts account, String type, Cards card) {
-		this.amount = amount;
-		this.type = type;
-		this.account = account;
-		this.card = card;
-		
-		if (type.equalsIgnoreCase("Deposit")) {
-			account.setBalance(amount + account.balance);
-		}
-		
-		else if (type.equalsIgnoreCase("Withdrawal")) {
-			account.setBalance(account.balance - amount);
-		}
-		
-		else if (type.equalsIgnoreCase("Purchase")) {
-			account.setBalance(account.balance - amount);
-		}
-		
-		account.transactions.add(this);
-		card.transactions.add(this);
-		
-	}
-	
-	public Transactions(double amount, Accounts account, String type) {
-		this.amount = amount;
-		this.type = type;
-		this.account = account;
-	}
-	
-	
-	
+    double amount;
+    String type; // Purchase, Deposit, Withdrawal, Transfer
+    Accounts account;
+    Cards card;
+
+    public Transactions(double amount, Accounts account, String type, Cards card) {
+        this.amount = amount;
+        this.type = type;
+        this.account = account;
+        this.card = card;
+
+        if (type.equalsIgnoreCase("Deposit")) {
+            account.setBalance(account.getBalance() + amount);
+        } else if (type.equalsIgnoreCase("Withdrawal") || type.equalsIgnoreCase("Purchase")) {
+
+            if (amount > account.getBalance()) {
+                throw new IllegalArgumentException("Insufficient funds");
+            }
+
+            account.setBalance(account.getBalance() - amount);
+        }
+
+        account.transactions.add(this);
+
+        if (card != null) {
+            card.transactions.add(this);
+        }
+    }
 }
