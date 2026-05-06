73471c55b3c3add8f7cc311b5df58725805299a0
kbdunn07 <157818211+kbdunn07@users.noreply.github.com>
2026-05-06 17:13:20 -0500

    Update Database.java

--- Database.java
+++ Database.java
@@ -1,1 +1,29 @@
+package BankingPackage;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+public class Database {
+
+    public static Connection connect() {
+        try {
+            return DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/banking_system",
+                    "root",
+                    "g8m3t1m3"
+            );
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+        Connection conn = Database.connect();
+
+        if (conn != null) {
+            System.out.println("Connected successfully!");
+        } else {
+            System.out.println("Connection failed.");
+        }
+    }
+}
